f762be2d06779867d701a18aed76ce29bb77a65e
Matt Osborne <MattJOzzy@gmail.com>
2023-04-05 16:45:32 -0500

    worked on navbar media queries and other styling

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -8,6 +8,7 @@ import Auth from '../../utils/auth';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
+import {AiOutlineShoppingCart} from 'react-icons/ai'
 
 const stripePromise = loadStripe('pk_test_51MtYp9Ez0mh6YSVXTKg8oYT8P8KB2aoOiktzz41yMRgMW7GS7zsipzLFNrbQbPSOEzpnH1Z3PoahcoFnOoLiFht000PIF5lKYj');
 
@@ -65,9 +66,7 @@ const Cart = () => {
   if (!state.cartOpen) {
     return (
       <div className="cart-closed" onClick={toggleCart}>
-        <span role="img" aria-label="trash">
-          Cart
-        </span>
+          <AiOutlineShoppingCart />
       </div>
     );
   }
