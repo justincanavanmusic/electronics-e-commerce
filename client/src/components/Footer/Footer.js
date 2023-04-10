1eabc2f988f1ca49f010db5a22e2be79453e8b13
R-Rangel <r.rangel.0222@gmail.com>
2023-04-10 09:47:24 -0500

    added max height to footer so that it doesnt overlap content

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,7 +5,9 @@ import { FcElectronics } from 'react-icons/fc'
 
 function Footer() {
     return (
-     
+        
+     <div className="btm-footer">
+
 <div className="d-flex fixed-bottom align-items-center footer-color justify-content-between sticky-bottom">
 <p className="pt-3 created-by">Site created by Dom, Matt, Ricardo, Justin</p>
 
@@ -13,6 +15,10 @@ function Footer() {
 
 <p className='contact pt-3'>Contact us for inquiries at gamercircuit@gmail.com!</p>
 </div>
+
+
+     </div>
+
 
     )
 }
